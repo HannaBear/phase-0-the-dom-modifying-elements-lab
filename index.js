00827a79160705515d1d9633a69efb0d78a52e9a
Hanna <marmalademarmoset@gmail.com>
2022-11-12 21:10:35 -0800

    commit tree 40eee0405b4167226041ad0d948ff67e096e627f

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+
+main.remove();
+
+const newHeader = document.createElement('h1')
+document.body.append(newHeader)
+document.querySelector('h1').id = 'victory'
+newHeader.textContent = "Hanna is the champion!"
+
+
